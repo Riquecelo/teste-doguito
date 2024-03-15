3a16bd025ead61cf543fc88fa47bd3188824c8cd
Marcelo Santos <marcelo.has@hotmail.com>
2024-03-15 14:41:52 -0300

    construção dos estilos globais

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './pages/App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from './styles/global';
+import theme from './styles/theme';
+
+import App from './pages/App';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles/>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
