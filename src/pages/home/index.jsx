bfaf0b625d5c935e91981009a95b7f47ace1d73e
Marcelo Santos <marcelo.has@hotmail.com>
2024-03-16 19:15:33 -0300

    adicionando componente de formulário

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 
+
 import { BreedSelector } from "../../components/BreedSelector";
 import { DogImage } from "../../components/DogImage";
+import { FormDog } from '../../components/FormDog';
 
 export function Home() {
 
@@ -10,12 +12,13 @@ export function Home() {
   const handleBreedSelect = (breed) => {
     setSelectedBreed(breed);
   };
+
   return (
     <>
-      
-      <h1>Doguito</h1>
       <BreedSelector onSelect={handleBreedSelect}/>
+      <FormDog/>
       <DogImage breed={selectedBreed}/>
+      
     </>
   )
 }
